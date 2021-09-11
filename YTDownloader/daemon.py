1d5ccf4361e2251f0e5afaed833a50f7a52c62f8
T4puSD <sdtapusd20@gmail.com>
2021-09-11 19:18:05 +0600

    refactored the file

--- YTDownloader/daemon.py
+++ YTDownloader/daemon.py
@@ -10,7 +10,6 @@ from YTDownloader.advancedyvdown import start_video_download
 from YTDownloader.advancedyvdown import start_high_quality_video_download
 from YTDownloader.Library.Threading.stoppableThread import StoppableThread
 
-# genererating configure.ini file at first to
 # load basic Configuration files
 config = get_configuration()
 
@@ -22,8 +21,9 @@ NUMBER_OF_THREADS = config['conf'].getint('number_of_threads')
 
 task_queue = queue.Queue()
 
+
 def reloadConfig():
-    # relaoding download dirctories
+    # reloading download directories
     # in advancedyvdonw.py script
     reloadDownloadDirs()
     global MEDIA_TYPE
@@ -32,7 +32,7 @@ def reloadConfig():
     global NUMBER_OF_THREADS
 
     config = get_configuration()
-    
+
     MEDIA_TYPE = config['media_conf'].get('media_type')
     MEDIA_QUALITY = config['media_conf'].get('media_quality')
     DOWNLOAD_MODE = config['conf'].get('download_mode')
@@ -40,9 +40,10 @@ def reloadConfig():
 
 
 class ClipBoardThread(StoppableThread):
-    def __init__(self, queue, *args,**kwargs):
+    def __init__(self, queue, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.queue = queue
+
     def run(self):
         MEDIA_TYPE = config['media_conf'].get('media_type')
         MEDIA_QUALITY = config['media_conf'].get('media_quality')
@@ -51,16 +52,18 @@ class ClipBoardThread(StoppableThread):
         prev_url = pyperclip.paste()
         while not self.stopped():
             current_url = pyperclip.paste()
-            if(prev_url != current_url):
+            if prev_url != current_url:
                 if current_url.startswith('https://www.youtube.com/watch?v=') and len(current_url.split("=")) > 1:
                     self.queue.put(current_url)
                     prev_url = current_url
 
+
 class WorkerThread(StoppableThread):
-    def __init__(self,queue,thread_name, *args, **kwargs):
+    def __init__(self, queue, thread_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.task_queue = queue
         self.thread_name = thread_name
+
     def run(self):
         while not self.stopped():
             try:
@@ -69,34 +72,36 @@ class WorkerThread(StoppableThread):
                 pass
             else:
                 try:
-                    if(MEDIA_TYPE == 'audio'):
+                    if MEDIA_TYPE == 'audio':
                         start_audio_download(url)
                         task_queue.task_done()
-                        logging.debug(self.thread_name+ " Completed")
-                    elif(MEDIA_TYPE == 'video'):
-                        if(MEDIA_QUALITY == 'normal'):
+                        logging.debug(self.thread_name + " Completed")
+                    elif MEDIA_TYPE == 'video':
+                        if MEDIA_QUALITY == 'normal':
                             start_video_download(url)
                             task_queue.task_done()
-                            logging.debug(self.thread_name+ " Completed")
-                        elif(MEDIA_QUALITY == 'hq'):
+                            logging.debug(self.thread_name + " Completed")
+                        elif MEDIA_QUALITY == 'hq':
                             start_high_quality_video_download(url)
                             task_queue.task_done()
-                            logging.debug(self.thread_name+ " Completed")
-                    if(task_queue.empty()):
+                            logging.debug(self.thread_name + " Completed")
+                    if task_queue.empty():
                         logging.debug("No pending task in the queue")
                 except Exception as e:
                     logging.debug("Error occured in daemon loop")
                     logging.debug(e)
+
 
 # mainThread
 mainThread = ClipBoardThread(task_queue)
 
 worker_thread_list = []
 for i in range(NUMBER_OF_THREADS):
-    t = WorkerThread(task_queue,f'Thread-{i}')
+    t = WorkerThread(task_queue, f'Thread-{i}')
     t.setDaemon(True)
     # t.start()
     worker_thread_list.append(t)
+
 
 def stopTheServers():
     result = False
@@ -110,7 +115,7 @@ def stopTheServers():
         for worker in worker_thread_list:
             if not worker.is_alive():
                 work_is_pending = False
-                
+
         # checking if workers are alive
         if work_is_pending:
             # stopping worker threads
@@ -122,6 +127,7 @@ def stopTheServers():
             #     worker.join()
     return result
 
+
 def resetTheThreads():
     # task_queue = queue.Queue()
     global mainThread
@@ -129,29 +135,31 @@ def resetTheThreads():
     mainThread = ClipBoardThread(task_queue)
     worker_thread_list.clear()
     for i in range(NUMBER_OF_THREADS):
-        t = WorkerThread(task_queue,f'Thread-{i}')
+        t = WorkerThread(task_queue, f'Thread-{i}')
         worker_thread_list.append(t)
+
 
 def sigint_handler(signum, frame):
     logging.debug('Shutting Threads down...')
     stopTheServers()
     sys.exit(0)
 
+
 def startTheServers():
-    logging.debug("Starting Daemon Server! Modes: {} {} {}".format(MEDIA_TYPE,MEDIA_QUALITY,DOWNLOAD_MODE))
+    logging.debug("Starting Daemon Server! Modes: {} {} {}".format(MEDIA_TYPE, MEDIA_QUALITY, DOWNLOAD_MODE))
     if mainThread.is_alive():
         return True
-    
+
     if not mainThread.is_alive():
-        #starting main thread
+        # starting main thread
         mainThread.start()
-    
+
     work_is_pending = True
     for worker in worker_thread_list:
         if not worker.is_alive():
             work_is_pending = False
     if not work_is_pending:
-        #starting worker threads
+        # starting worker threads
         for t in worker_thread_list:
             t.start()
         return False
@@ -162,6 +170,7 @@ def startTheServers():
     # print("stopping main thread")
     # mainThread.stop()
     # print("main thread is stopped")
+
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, sigint_handler)
